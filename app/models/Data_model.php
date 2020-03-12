243c7654e24f09108fd377925e72fd0b8bdb0082
Hachidaime <cold.kid40@gmail.com>
2020-03-12 15:46:58 +0700

    Update Data: code eficiency

--- app/models/Data_model.php
+++ app/models/Data_model.php
@@ -170,70 +170,65 @@ class Data_model extends Database
 
         $data = [];
 
-        $select = [
-            "{$jalan_table}.no_jalan",
-            "{$jalan_table}.nama_jalan",
-            "{$jalan_table}.kepemilikan",
-            "{$jalan_table}.panjang",
-            "{$jalan_table}.lebar_rata",
-            "{$koordinat_table}.koordinat_final AS koordinat"
+        $plain = [
+            'select' => [
+                "{$jalan_table}.no_jalan",
+                "{$jalan_table}.nama_jalan",
+                "{$jalan_table}.kepemilikan",
+                "{$jalan_table}.panjang",
+                "{$jalan_table}.lebar_rata",
+                "{$koordinat_table}.koordinat_final AS koordinat"
+            ],
+            'join' => [
+                "LEFT JOIN {$koordinat_table} ON {$koordinat_table}.no_jalan = {$jalan_table}.no_jalan"
+            ],
+            'sort' => [
+                "{$jalan_table}.kepemilikan ASC",
+                "{$jalan_table}.no_jalan ASC"
+            ],
+            'filter' => [
+                "{$jalan_table}.nama_jalan NOT LIKE '%test%'"
+            ]
         ];
-        $params['select'] = implode(", ", $select);
 
-        $join = [
-            "LEFT JOIN {$koordinat_table} ON {$koordinat_table}.no_jalan = {$jalan_table}.no_jalan"
-        ];
-        $params['join'] = implode(" ", $join);
-
-        $sort = [
-            "{$jalan_table}.kepemilikan ASC",
-            "{$jalan_table}.no_jalan ASC"
-        ];
-        $params['sort'] = implode(", ", $sort);
-
-        $filter = [
-            "{$jalan_table}.nama_jalan NOT LIKE '%test%'"
-        ];
-        $params['filter'] = implode(' ', $filter);
-
-        $query = $this->getSelectQuery($jalan_table, $params);
+        $query = $this->getSelectQuery($jalan_table, Functions::getParams($plain));
         $this->execute($query);
         list($data['jalan'],) = $this->multiarray();
 
-        $select = [
-            "{$jalan_table}.no_jalan",
-            "{$jalan_table}.nama_jalan",
-            "{$jalan_table}.kepemilikan",
-            "{$jalan_table}.panjang",
-            "{$jalan_table}.lebar_rata",
-            "{$detail_table}.no_detail",
-            "{$detail_table}.latitude",
-            "{$detail_table}.longitude",
-            "{$detail_table}.perkerasan",
-            "{$detail_table}.kondisi",
-            "{$detail_table}.segment",
-            "{$detail_table}.koordinat",
-            "{$foto_table}.row_id",
-            "{$foto_table}.foto"
+        $detail = [
+            'select' => [
+                "{$jalan_table}.no_jalan",
+                "{$jalan_table}.nama_jalan",
+                "{$jalan_table}.kepemilikan",
+                "{$jalan_table}.panjang",
+                "{$jalan_table}.lebar_rata",
+                "{$detail_table}.no_detail",
+                "{$detail_table}.latitude",
+                "{$detail_table}.longitude",
+                "{$detail_table}.perkerasan",
+                "{$detail_table}.kondisi",
+                "{$detail_table}.segment",
+                "{$detail_table}.koordinat",
+                "{$foto_table}.row_id",
+                "{$foto_table}.foto"
+            ],
+            'join' => [
+                "LEFT JOIN {$jalan_table} ON {$jalan_table}.no_jalan = {$detail_table}.no_jalan",
+                "LEFT JOIN {$foto_table} ON ({$foto_table}.latitude = {$detail_table}.latitude AND {$foto_table}.longitude = {$detail_table}.longitude)"
+            ],
+            'sort' => [
+                "{$detail_table}.no_jalan ASC",
+                "{$detail_table}.no_detail ASC",
+                "{$detail_table}.perkerasan ASC",
+                "{$detail_table}.kondisi ASC",
+                "{$detail_table}.segment ASC"
+            ],
+            'filter' => [
+                "{$jalan_table}.nama_jalan NOT LIKE '%test%'"
+            ]
         ];
-        $params['select'] = implode(", ", $select);
-
-        $join = [
-            "LEFT JOIN {$jalan_table} ON {$jalan_table}.no_jalan = {$detail_table}.no_jalan",
-            "LEFT JOIN {$foto_table} ON ({$foto_table}.latitude = {$detail_table}.latitude AND {$foto_table}.longitude = {$detail_table}.longitude)"
-        ];
-        $params['join'] = implode(" ", $join);
-
-        $sort = [
-            "{$detail_table}.no_jalan ASC",
-            "{$detail_table}.no_detail ASC",
-            "{$detail_table}.perkerasan ASC",
-            "{$detail_table}.kondisi ASC",
-            "{$detail_table}.segment ASC"
-        ];
-        $params['sort'] = implode(", ", $sort);
-
-        $query = $this->getSelectQuery($detail_table, $params);
+        $query = $this->getSelectQuery($detail_table, Functions::getParams($detail));
+        // echo $query . "<br>";
         $this->execute($query);
         list($data['detail'],) = $this->multiarray();
 
