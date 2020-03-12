2a8794602ff6251c3f651d73888518080efc3ef2
Hachidaime <cold.kid40@gmail.com>
2020-03-12 14:36:59 +0700

    Update Gis: change sidepanel tab positon

--- app/views/Gis/index.php
+++ app/views/Gis/index.php
@@ -2,16 +2,16 @@
     <div id="mySidepanel" class="sidepanel border bg-white">
         <!-- Nav tabs -->
         <ul class="nav nav-tabs nav-fill clr-2018-coconut-milk border-bottom">
-            <li class="nav-item">
-                <a class="nav-link active" href="#legend">Legend</a>
+            <li class="nav-item order-2">
+                <a class="nav-link" href="#legend">Legend</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#cari">Cari</a>
+            <li class="nav-item order-1">
+                <a class="nav-link active" href="#cari">Cari</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item order-3">
                 <a class="nav-link" href="#info">Info</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item order-4">
                 <a href="javasript:void(0)" class="close btn-sidebar-close p-1" aria-label="Close">
                     <i class="material-icons">close</i>
                 </a>
@@ -20,8 +20,9 @@
 
         <!-- Tab panes -->
         <div class="tab-content">
-            <div id="legend" class="tab-pane active p-3">
+            <div id="legend" class="tab-pane fade p-3">
                 <div class="d-flex flex-column">
+                    {*
                     <div class="pr-3 py-1">
                         <svg height="18" width="20">
                             <polygon points="0,8 0,14 18,14 18,8" style="fill:#00b050;"></polygon>
@@ -64,9 +65,10 @@
                         </svg>
                         Titik Awal Ruas Jalan
                     </div>
+                    *}
                 </div>
             </div>
-            <div id="cari" class="tab-pane fade py-3">
+            <div id="cari" class="tab-pane py-3 active">
                 {$data.searchform}
                 <div class=" search-btn d-flex justify-content-center mb-3">
                     {foreach from=$data.searchbtn key=k item=v}
