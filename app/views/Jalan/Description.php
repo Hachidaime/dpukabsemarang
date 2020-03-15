582a12ed3e598028d910e3059ff90d2ffd4c9d13
Hachidaime <cold.kid40@gmail.com>
2020-03-15 12:34:27 +0700

    Update Jalan: create views Jalan/Description

---
+++ app/views/Jalan/Description.php
@@ -0,0 +1,21 @@
+<table>
+    <tr>
+        <td colspan="2"></td>
+    </tr>
+    <tr>
+        <td>No. Ruas Jalan</td>
+        <td>{$data.no_jalan}</td>
+    </tr>
+    <tr>
+        <td>Nama Ruas Jalan</td>
+        <td>{$data.nama_jalan}</td>
+    </tr>
+    <tr>
+        <td>Panjang</td>
+        <td>{$data.panjang} km</td>
+    </tr>
+    <tr>
+        <td>Lebar</td>
+        <td>{$data.lebar} m</td>
+    </tr>
+</table>
