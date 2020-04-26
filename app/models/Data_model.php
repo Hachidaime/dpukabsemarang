ef0e26b8a88b34380444379999e6e880560e51aa
Hachidaime <cold.kid40@gmail.com>
2020-04-26 14:25:07 +0700

    Update DataJalan: prepare data jalan

--- app/models/Data_model.php
+++ app/models/Data_model.php
@@ -5,6 +5,18 @@
  */
 class Data_model extends Database
 {
+    public function getDataThead()
+    {
+        Functions::setDataSession('thead', ['0', 'row', '#']);
+        Functions::setDataSession('thead', ['0', 'no_jalan', 'Nomor Ruas Jalan', 'data-halign="center" data-align="center" data-width="150"']);
+        Functions::setDataSession('thead', ['0', 'nama_jalan', 'Nama Ruas Jalan', 'data-halign="center" data-align="left"']);
+        Functions::setDataSession('thead', ['0', 'kepemilikan', 'Status Kepemilikan', 'data-halign="center" data-align="left" data-width="200"']);
+        Functions::setDataSession('thead', ['0', 'panjang', 'Panjang<br>(m)', 'data-halign="center" data-align="left" data-width="120"']);
+        Functions::setDataSession('thead', ['0', 'lebar_rata', 'Lebar<br>Rata-Rata (m)', 'data-halign="center" data-align="left" data-width="120"']);
+        Functions::setDataSession('thead', ['0', 'operate']);
+        return Functions::getDataSession('thead');
+    }
+
     public function getAllDataJalan()
     {
         $jalan_model = $this->model('Jalan_model');
