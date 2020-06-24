0b44ebfce7c8bb6c4c9ba3f4054737076af341c1
Hachidaime <cold.kid40@gmail.com>
2020-06-25 05:22:34 +0700

    Update Laporan: generate pdf

---
+++ app/views/Layout/PdfLayout.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <style>
+    table {
+      border-collapse: collapse;
+    }
+
+    table,
+    td,
+    th {
+      border: 1px solid black;
+    }
+
+    .align-top {
+      vertical-align: top !important;
+    }
+
+    .text-center {
+      text-align: center;
+    }
+
+    .text-right {
+      text-align: right;
+    }
+  </style>
+</head>
+
+<body>
+  {$content}
+</body>
+
+</html>
